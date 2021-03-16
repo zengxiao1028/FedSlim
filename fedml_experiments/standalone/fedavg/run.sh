431180908b9c5bb9e50a4553ac1002f0330e4f14
xiaozeng <zengxiao1028@gmail.com>
2021-03-16 11:10:06 -0400

    update script

--- fedml_experiments/standalone/fedavg/run.sh
+++ fedml_experiments/standalone/fedavg/run.sh
@@ -10,4 +10,6 @@
 #python main_fedavg.py --exp_name=data_staleIS_only_6000clients --lr_decay=cosine --use_data_IS --stale_IS_weight
 #python main_fedavg.py --exp_name=standard_3000comrounds --lr_decay=cosine --comm_round=3000
 
-python main_fedavg.py --exp_name=slimmable --lr_decay=cosine --slim_training --wandb_dir=/home/xiao/projects/FedSlim/results
+#python main_fedavg.py --exp_name=slimmable --lr_decay=cosine --slim_training --wandb_dir=/home/xiao/projects/FedSlim/results
+
+python main_fedavg.py --exp_name=fedslim_adam --lr_decay=cosine --slim_training --slim_width 0.25 0.5 0.75 1.0 --slim_channels=leftmost --server_optimizer=adam --epochs=1
